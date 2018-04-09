d21d0a9bfe600b13561b5a5e4e3568937a082b1b
mbwana.mbura <mbwanajm@gmail.com>
2018-04-09 17:23:38 +0300

    initial commit

---
+++ core/src/main/java/com/niafikra/inaya/InayaApplication.java
@@ -0,0 +1,11 @@
+package com.niafikra.inaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InayaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InayaApplication.class, args);
+    }
+}
